874833b5eb3a27185ec9d4d22e4af71178d96768
bhaskardutta111 <38983349+bhaskardutta111@users.noreply.github.com>
2020-04-10 12:23:37 +0530

    thirdCommit-added slider

--- js/script.js
+++ js/script.js
@@ -1,46 +1,59 @@
-var counter = document.querySelectorAll(".counter-data")
+// var counter = document.querySelectorAll(".counter-data")
 
-window.addEventListener("load", function() {
-    counter.forEach(function(x, y) {
+// window.addEventListener("load", function() {
+//     counter.forEach(function(x, y) {
 	
-    var start = counter[y].getAttribute('data-count-start')
-    var end = counter[y].getAttribute('data-count-end')
-    var speed = counter[y].getAttribute('data-speed')
+//     var start = counter[y].getAttribute('data-count-start')
+//     var end = counter[y].getAttribute('data-count-end')
+//     var speed = counter[y].getAttribute('data-speed')
 
-    setInterval(function() {
-        start++;
-        if(start > end) {
-        return false;
-        }
-        counter[y].innerText = start;
-    }, speed)
-})}, false)
+//     setInterval(function() {
+//         start++;
+//         if(start > end) {
+//         return false;
+//         }
+//         counter[y].innerText = start;
+//     }, speed)
+// })}, false)
 
 
-var collapseBtn = document.querySelector(".collapseBtn");
+// var collapseBtn = document.querySelector(".collapseBtn");
 
-for (var i = 0; i < collapseBtn.length; i++) {
-    collapseBtn[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = document.querySelector(".btnContent");
-    if (content.style.maxHeight){
-        content.style.maxHeight = null;
-    } else {
-        content.style.maxHeight = content.scrollHeight + "px";
-    } 
-    });
-}
+// for (var i = 0; i < collapseBtn.length; i++) {
+//     collapseBtn[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//     var content = document.querySelector(".btnContent");
+//     if (content.style.maxHeight){
+//         content.style.maxHeight = null;
+//     } else {
+//         content.style.maxHeight = content.scrollHeight + "px";
+//     } 
+//     });
+// }
 
-var slideIndex = 0;
+var slideInd = 0;
 showSlides();
+
 
 function showSlides() {
     var slides = document.getElementsByClassName("slide");
+    var dots = document.getElementsByClassName("dot");
+
     for (var z = 0; z < slides.length; z++) {
     slides[z].style.display = "none";
     }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
-    slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
+
+    slideInd++;
+    if (slideInd > slides.length) {
+        slideInd = 1;
+    }
+    slides[slideInd - 1].style.display = "block";
+    setTimeout(showSlides, 4000);
+
+    for (var x = 0; x < dots.length; x++) {
+        dots[x].className = dots[x].className.replace(" active", "");
+    }
+    dots[slideInd-1].className += " active";
+
 }
+
