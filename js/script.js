87d9aa9bb25f6a5253f1c4bbffec97fcb77d9c6f
bhaskardutta111 <38983349+bhaskardutta111@users.noreply.github.com>
2020-04-11 17:33:50 +0530

    seventhCommit-updated styles and scripts

--- js/script.js
+++ js/script.js
@@ -1,21 +1,18 @@
-// var counter = document.querySelectorAll(".counter-data")
 
-// window.addEventListener("load", function() {
-//     counter.forEach(function(x, y) {
-	
-//     var start = counter[y].getAttribute('data-count-start')
-//     var end = counter[y].getAttribute('data-count-end')
-//     var speed = counter[y].getAttribute('data-speed')
-
-//     setInterval(function() {
-//         start++;
-//         if(start > end) {
-//         return false;
-//         }
-//         counter[y].innerText = start;
-//     }, speed)
-// })}, false)
-
+//change navbar bg on scroll
+window.onscroll = function() {
+    var fixedTop = document.querySelector('.fixed-top');
+    var scrollTop = window.scrollY;
+    if(scrollTop > 10 && scrollTop < 50){
+        fixedTop.style.opacity = '0.8';
+        fixedTop.style.margin = '0';
+    } else if (scrollTop > 50) {
+        fixedTop.style.background = '#333';
+    }
+    else {
+        fixedTop.style.background = 'transparent';
+    }
+};
 
 var collapseBtn = document.getElementsByClassName("collapseBtn");
 
@@ -58,3 +55,22 @@ function showSlides() {
 
 }
 
+
+
+// var counter = document.querySelectorAll(".counter-data")
+
+// window.addEventListener("load", function() {
+//     counter.forEach(function(x, y) {
+	
+//     var start = counter[y].getAttribute('data-count-start')
+//     var end = counter[y].getAttribute('data-count-end')
+//     var speed = counter[y].getAttribute('data-speed')
+
+//     setInterval(function() {
+//         start++;
+//         if(start > end) {
+//         return false;
+//         }
+//         counter[y].innerText = start;
+//     }, speed)
+// })}, false)
