7ab9387b1433f309f9e632df31f7d2a5e2342ce5
bhaskardutta111 <38983349+bhaskardutta111@users.noreply.github.com>
2020-04-20 13:25:44 +0530

    finalCommit01-added blogs section lgScreen

--- js/carousel.js
+++ js/carousel.js
@@ -19,4 +19,25 @@ $(document).ready(function(){
         }
         }]
     });
+    $('.blog-slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false,
+        dots: false,
+        pauseOnHover: true,
+        responsive: [{
+        breakpoint: 768,
+        settings: {
+            slidesToShow: 4
+        }
+        }, {
+        breakpoint: 520,
+        settings: {
+            slidesToShow: 3
+        }
+        }]
+    });
 });
+
